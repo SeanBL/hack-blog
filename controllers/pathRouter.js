55c08aa7859b867d8156c93f8dd52fd6c76dd755
SeanBristol-Lee <seanbristol81@gmail.com>
2023-01-12 02:45:41 -0800

    create login template

--- controllers/pathRouter.js
+++ controllers/pathRouter.js
@@ -1,9 +1,14 @@
 const { Router} = require('express');
+const path = require('path');
 
 const pathRouter = new Router;
 
 pathRouter.get('/', (req, res) => {
     res.render('home');
 });
+
+pathRouter.get('/login', (req, res) => {
+    res.render('login');
+})
 
 module.exports = pathRouter;
