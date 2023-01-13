f273dfe6f34ba62e2d9d8506b475dcaf98cbe001
SeanBristol-Lee <seanbristol81@gmail.com>
2023-01-13 04:00:42 -0800

    add post route for login

--- public/js/login.js
+++ public/js/login.js
@@ -4,4 +4,15 @@ document.getElementById('login_form').addEventListener("submit", (event) => {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     console.log(username, password);
+
+    fetch("/api/user/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username,
+            password,
+        }),
+    });
 });
