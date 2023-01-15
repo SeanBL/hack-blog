5306732d903bd28d8027c1f468bd58aa8f4c384b
SeanBristol-Lee <seanbristol81@gmail.com>
2023-01-14 22:10:42 -0800

    add user model

--- controllers/apis/users.js
+++ controllers/apis/users.js
@@ -1,11 +1,45 @@
 const { Router } = require("express");
 
+const User = require('../../models/user');
+
 const usersRouter = new Router();
 
 usersRouter.post("/login", async (req, res) => {
-    console.log(req.body);
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ where: { username, }});
+
+    if (!user) {
+        res.status(401).end('User not found');
+        return;
+    }
+
+    if (user.password != password) {
+        res.status(401).end("Bad password");
+        return;
+    }
 
     res.end();
 })
+
+usersRouter.post("/", async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ where: { username, }});
+
+    if (user) {
+        res.status(409).end('User already exists');
+        return;
+    }
+
+    const newUserObject = await User.create({
+        username,
+        password,
+    });
+
+    res.status(200).json({
+        id: newUserObject.id,
+    });
+});
 
 module.exports = usersRouter;
--- public/js/login.js
+++ public/js/login.js
@@ -5,6 +5,7 @@ document.getElementById('login_form').addEventListener("submit", (event) => {
     const password = document.getElementById("password").value;
     console.log(username, password);
 
+    document.getElementById("errors").textContent = "";
     fetch("/api/user/login", {
         method: 'POST',
         headers: {
@@ -14,5 +15,39 @@ document.getElementById('login_form').addEventListener("submit", (event) => {
             username,
             password,
         }),
-    });
+    }).then((result) => {
+        if (!result.ok) {
+            document.getElementById("errors").textContent = "Unable to login";
+            return null;
+        } else {
+            return result.json();
+        }
+    })
+});
+
+document.getElementById('signup_form').addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username_register").value;
+    const password = document.getElementById("password_register").value;
+    console.log(username, password);
+
+    document.getElementById("signup_errors").textContent = "";
+    fetch("/api/user", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username,
+            password,
+        }),
+    }).then((result) => {
+        if (!result.ok) {
+            document.getElementById("signup_errors").textContent = "Unable to create user";
+            return null;
+        } else {
+            return result.json();
+        }
+    })
 });
