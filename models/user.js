c4ff01f81223dc852ab7cd141679276db3ce6544
SeanBristol-Lee <seanbristol81@gmail.com>
2023-01-24 02:43:50 -0800

    add encryption

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const bcrypt = require("bcrypt");
 
 class User extends Model {};
 
@@ -25,7 +26,13 @@ User.init(
         sequelize,
         freezeTableName: true,
         underscored: true,
-        modelName: 'user'
+        modelName: 'user',
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        }
     }
 );
 
