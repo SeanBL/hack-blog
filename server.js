c9b73f70a198591e860bece18c11760137e84c72
SeanBristol-Lee <seanbristol81@gmail.com>
2023-01-29 15:40:36 -0800

    slight changes to handlebars

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require('cookie-parser');
 
-
 const { engine } = require('express-handlebars');
 const path = require('path');
 
 const sequelize = require('./config/connection');
-
 
 const mainRouter = require("./controllers");
 const { Sequelize } = require("sequelize");
