83a6ac61d13626a128082dd2e127a86f5121db2b
SeanBristol-Lee <seanbristol81@gmail.com>
2023-01-29 01:45:59 -0800

    fix connections.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-    process.env.DB_DB,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        port: 3306,
-    },
-);
+const mysql = require("mysql2");
+
+require('dotenv').config();
+let sequelize;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+    sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL)
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_DB,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'mysql',
+            port: 3306,
+        },
+    );
+}
+
 
 module.exports = sequelize;
